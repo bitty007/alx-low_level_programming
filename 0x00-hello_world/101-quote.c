89043b4b136c1bab84ce94d445a8b17ed21493aa
Bitrus Abeson Praise <bitruspraise@gmail.com>
2022-09-08 15:20:48 -0700

    0x00. C - Hello, World

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,24 @@
+#!/bin/bash 
+#include <stdio.h>
+
+#include <string.h>
+
+#include <unistd.h>
+
+
+
+int main (void)
+
+{
+
+	 int s = strlen("and that piece of art is useful\" - Dora Korpar,2015-10-19\n");
+
+	 write(2,
+
+			   "and that piece of art is useful\" - Dora Korpar,2015-10-19\n",s);
+
+
+
+	 return (1);
+
+}
