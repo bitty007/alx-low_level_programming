dbfa2522343b65be863bec4e4d80f95fead69449
Bitrus Abeson Praise <bitruspraise@gmail.com>
2022-09-08 16:29:25 -0700

    0x00. C - Hello, World

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-
-int main (void)
-
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
 {
-
-	printf("with proper grammar,but the outcome is a piece of art,\n");
-
+	printf("with proper grammar, but the outcome is a piece of art,\n";
 	return (0);
-
-}
+	}	
