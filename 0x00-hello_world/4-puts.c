2e11cc8bc0ad0dde4c9fec222a199d0c36e78cf9
Bitrus Abeson Praise <bitruspraise@gmail.com>
2022-09-08 16:11:30 -0700

    0x00. C - Hello, World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+
 /**
- * main - main block
+ * main - print the string in the put fuction
  *
  * Descirption: using the main function 
- * this program prints "programming is like building a multilingual puzzle
+ * this program prints "Programming is like building a multilingual puzzle
  * return 0
  */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
