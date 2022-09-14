32c491cf5529c3febb7e5b5f43b4a54c8b3f8236
Bitrus Abeson Praise <bitruspraise@gmail.com>
2022-09-14 12:32:20 -0700

    task  commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#include "main.h" 
+/**
+ * _abs - computes the absolute value of an integer
+ * @ab: input letrer
+ * Return: Always sucess
+ */
+int _abs(int ab)
+{
+
+return (ab * ((ab > 0) - (ab < 0)));
+}
