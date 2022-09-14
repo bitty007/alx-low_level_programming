3bba2d876d40255c57bd6abf57cbcea3b15e6c89
Bitrus Abeson Praise <bitruspraise@gmail.com>
2022-09-14 14:09:16 -0700

    task  commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,11 +7,12 @@
 
 void print_alphabet(void)
 {
-	char c;
 
-	for (c = 'a'; c <= 'z'; c++)
-	{
-		_putchar(c);
-	}	
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}	
 _putchar('\n');
 }
