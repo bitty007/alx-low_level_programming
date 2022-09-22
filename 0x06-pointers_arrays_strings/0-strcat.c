aa2f2c725114b8c95344c517dc074390337dc74b
Bitrus Abeson Praise <bitruspraise@gmail.com>
2022-09-22 16:16:30 -0700

    tasks 0x06. C - More pointers, arrays and strings commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,23 +1,31 @@
 #include "main.h"
 
 /**
- * _strcat - concatenates the string pointed to by @src to
- * the end of the string pointed out by @dest
- * @dest: string that will be appended
+ * _strcat - joins two string
+ * @dest: copy to
+ * @src: copy from
  *
- * Return: returns pointer to @dest
+ * Return: a pointer resulting to a string
  */
 
 char *_strcat(char *dest, char *src)
 {
+	int i;
+	int j;
 
-	int index = 0, dest_len = 0;
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++
+		j++
+	}
 
-	while (dest[index++])
-		dest_len++;
-
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
-
+	dest[i] = '\0';
 	return (dest);
 }
