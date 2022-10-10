5c1dfea133f75898de6c125b41bf571157f9844e
Bitrus Abeson Praise <bitruspraise@gmail.com>
2022-10-10 13:08:51 -0700

    0x0E. C - Structures, typedef

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -19,7 +19,7 @@ void print_dog(struct dog *d)
 		printf("Age: %f\n", d->age);
 
 		if (!(d->owner))
-			printf("Owner: (nill)\n");
+			printf("Owner: (nil)\n");
 		else
 			printf("Owner: %s\n", d->owner);
 	}
