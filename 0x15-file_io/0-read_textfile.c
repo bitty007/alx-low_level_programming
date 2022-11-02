d31c84565acf52b225f8f573620485ca9035c207
prazido.bitty <111038897+bitty007@users.noreply.github.com>
2022-11-02 07:14:10 +0100

    commit 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,32 +1,32 @@
 #include "main.h"
-
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output
- * @filename: pointer to text in a file
- * @letters: number of letters
- * Return: the actual number of letters it could read and print
+ * read_textfile - Reads a textfile and prints the contents to the POSIX STDOUT
+ * @filename: The name of the file to read from
+ * @letters: The number of characters it should print to the STDOUT
+ * Return: Returns the number of characters printed
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    ssize_t file, fread, fwrite;
-    char *totalSize;
+	ssize_t fild = 0, chk = 0;
+	char *buffer;
 
-    totalSize = malloc(sizeof(char) * letters);
-    if (totalSize == NULL)
-        return (0);
-    if (filename == NULL)
-        return (0);
+	if (!filename || !letters)
+		return (0);
 
-    file = open(filename, O_RDONLY);
-    if (file == -1)
-        return (0);
-    fread = read(file, totalSize, letters);
-    if (fread == -1)
-        return (0);
-    fwrite = write(STDOUT_FILENO, totalSize, fread);
-    if (fwrite == -1)
-        return (0);
-    close(file);
-    free(totalSize);
-    return (fwrite);
+	fild = open(filename, O_RDONLY);
+	if (fild < 0)
+		return (0);
+
+	buffer = malloc(sizeof(char) * (letters));
+	if (!buffer)
+		return (0);
+
+	chk = read(fild, buffer, letters);
+	chk = write(STDOUT_FILENO, buffer, chk);
+	if (chk < 0)
+		return (0);
+
+	close(fild);
+	free(buffer);
+	return (chk);
 }
