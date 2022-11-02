fa33d08e745967123c41fbca5105c17f9796b13a
prazido.bitty <111038897+bitty007@users.noreply.github.com>
2022-11-02 07:19:38 +0100

    commit 4

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,29 +1,30 @@
 #include "main.h"
-
 /**
- * append_text_to_file - appends text at the end of a file
- * @filename: the name of the file
- * @text_content: the NULL terminated string to add at end of file
- * Return: 1 on success, -1 on failure
+ * append_text_to_file - This function adds text at EOF
+ * @filename: name of the file to append data to.
+ * @text_content: data to append to file
+ * Return: 1 if succesfull else -1 on err
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int file, fwrite, i;
+	int fild = 0, chk = 0, i = 0;
 
-    if (filename == NULL)
-        return (-1);
-    file = open(filename, O_APPEND | O_RDWR, 0664);
-    if (file == -1)
-        return (-1);
+	if (!filename)
+		return (-1);
 
-    if (text_content != NULL)
-    {
-        for (i = 0; text_content[i]; i++)
-            ;
-        fwrite = write(file, text_content, i);
-        if (fwrite == -1)
-            return (-1);
-    }
-    close(file);
-    return (1);
+	fild = open(filename, O_WRONLY | O_APPEND);
+	if (fild < 0)
+		return (-1);
+
+	if (text_content)
+	{
+		while (text_content[i])
+			i++;
+
+		chk = write(fild, text_content, i);
+		if (chk < 0)
+		return (-1);
+	}
+	close(fild);
+	return (1);
 }
